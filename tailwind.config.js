4083eb6bc27579ef71a529edc8b635a9947bf78f
Vashist-Agarwalla <vashist1202@gmail.com>
2021-09-30 22:24:47 +0530

    ✨ Add Colors and Fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@ module.exports = {
   purge: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        BGMain: "#2C291E",
+        TextMain: "#F4F0E1",
+        TextSub: "#F74700",
+      },
+      fontFamily: {
+        'primary': ['Lato'],
+      },
+    },
   },
   variants: {
     extend: {},
