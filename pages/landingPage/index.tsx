ff9ab81dcb33dfcd008c0816644b831ecf5754fe
Abhinav0909 <abhinavkarforma2@gmail.com>
2021-10-04 11:07:47 +0530

    ✨ Add landingPage

---
+++ pages/landingPage/index.tsx
@@ -0,0 +1,10 @@
+import LandingPage from "../../components/LandingPage/landingPage";
+const HeroPage = () => {
+    return (
+        <div className="bg-BGMain w-full h-full">
+         <LandingPage/>
+        </div>
+    )
+}
+
+export default HeroPage;
