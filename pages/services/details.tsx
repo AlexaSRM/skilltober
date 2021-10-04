7ec7e7a7e8ab12e2feaff927f60074e6a7f7178e
Abhinav0909 <abhinavkarforma2@gmail.com>
2021-10-04 11:11:39 +0530

    ✨ Add servicesPage

---
+++ pages/services/details.tsx
@@ -0,0 +1,10 @@
+import Services from "../../components/ServicesComp/services";
+const Details = () => {
+    return (
+        <div className="bg-BGMain w-full h-full ">
+            <Services />
+        </div>
+    )
+}
+
+export default Details;
