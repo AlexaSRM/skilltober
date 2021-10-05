091945461b5dcd25e99cd2753f3dd72e4bf9f7b6
Vashist-Agarwalla <vashist1202@gmail.com>
2021-10-05 08:42:59 +0530

    💄 Logos

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,17 +1,35 @@
+import Instagram from '../../public/assests/Instagram.svg'
+import LinkedIn from '../../public/assests/LinkedIn.svg'
+import Mail from '../../public/assests/Mail.svg'
+
 const Footer = () => {
     return(
         <div className='flex text-TextMain text-center py-6'>
             <div className='flex-1 mx-10'>
                 <h1 className='text-2xl'>Alexa Dev SRM</h1>
                 <h2 className='text-md'>alexadevsrm.com</h2>
+                <div className='grid grid-cols-3 mt-4 px-10'>
+                    <Instagram className='mx-10'/>
+                    <LinkedIn className='mx-10'/>
+                    <Mail className='mx-10'/>
+                </div>
             </div>
             <div className='flex-1 mx-10'>
                 <h1 className='text-2xl'>School of Computing</h1>
                 <h2 className='text-md'>SRM Institute of Science and Technology</h2>
+                <div className='grid grid-cols-2 mt-4 px-20'>
+                    <Instagram className='mx-10'/>
+                    <LinkedIn className='mx-10'/>
+                </div>
             </div>
             <div className='flex-1 mx-10'>
                 <h1 className='text-2xl'>Github Community SRM</h1>
                 <h2 className='text-md'>githubsrm.tech</h2>
+                <div className='grid grid-cols-3 mt-4 px-10'>
+                    <Instagram className='mx-10'/>
+                    <LinkedIn className='mx-10'/>
+                    <Mail className='mx-10'/>
+                </div>
             </div>
         </div>
     )
