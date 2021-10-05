7ca7d1838ed4298241179c2535fed81c8a3d1858
Vashist-Agarwalla <vashist1202@gmail.com>
2021-10-05 21:27:36 +0530

    💄 Update Spacing

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -6,44 +6,44 @@ const Footer = () => {
   return (
     <div className="lg:flex text-textPrimary text-center py-6">
       <div className="lg:flex-1 mx-10">
-        <h1 className="text-2xl">Alexa Developers SRM</h1>
+        <h1 className="text-xl">Alexa Developers SRM</h1>
         <h2 className="text-md">alexadevsrm.com</h2>
-        <div className="grid grid-cols-3 mt-4 px-14">
-          <a href="https://www.instagram.com/alexadevsrm/">
-            <Instagram className="lg:mx-10 mx-4" />
+        <div className="grid grid-cols-3 mt-4 w-8/12 mx-auto">
+          <a href="https://www.instagram.com/alexadevsrm/" target="_blank" rel="noopener noreferrer">
+            <Instagram className="mx-auto" />
           </a>
-          <a href="https://www.linkedin.com/company/alexa-developers-srm/">
-            <LinkedIn className="lg:mx-10 mx-4" />
+          <a href="https://www.linkedin.com/company/alexa-developers-srm/" target="_blank" rel="noopener noreferrer">
+            <LinkedIn className="mx-auto" />
           </a>
           <a href="mailto:hello@alexadevsrm.com">
-            <Mail className="lg:mx-10 mx-4" />
+            <Mail className="mx-auto" />
           </a>
         </div>
       </div>
       <div className="lg:flex-1 lg:mt-0 mt-5 mx-10">
-        <h1 className="text-2xl">School of Computing</h1>
+        <h1 className="text-xl">School of Computing</h1>
         <h2 className="text-md">SRM Institute of Science and Technology</h2>
-        <div className="grid grid-cols-2 mt-4 px-24">
-          <a href="https://www.instagram.com/schoolofcomputingsrmist/">
-            <Instagram className="lg:mx-10 mx-4" />
+        <div className="grid grid-cols-2 mt-4 w-5/12 mx-auto">
+          <a href="https://www.instagram.com/schoolofcomputingsrmist/" target="_blank" rel="noopener noreferrer">
+            <Instagram className="mx-auto" />
           </a>
-          <a href="https://www.linkedin.com/company/school-of-computing-srmist/">
-            <LinkedIn className="lg:mx-10 mx-4" />
+          <a href="https://www.linkedin.com/company/school-of-computing-srmist/" target="_blank" rel="noopener noreferrer">
+            <LinkedIn className="mx-auto" />
           </a>
         </div>
       </div>
       <div className="lg:flex-1 lg:mt-0 mt-5 mx-10">
-        <h1 className="text-2xl">Github Community SRM</h1>
+        <h1 className="text-xl">Github Community SRM</h1>
         <h2 className="text-md">githubsrm.tech</h2>
-        <div className="grid grid-cols-3 mt-4 px-14">
-          <a href="https://www.instagram.com/githubsrm/">
-            <Instagram className="lg:mx-10 mx-4" />
+        <div className="grid grid-cols-3 mt-4 w-8/12 mx-auto">
+          <a href="https://www.instagram.com/githubsrm/" target="_blank" rel="noopener noreferrer">
+            <Instagram className="mx-auto" />
           </a>
-          <a href="https://www.linkedin.com/company/githubsrm/mycompany/">
-            <LinkedIn className="lg:mx-10 mx-4" />
+          <a href="https://www.linkedin.com/company/githubsrm/mycompany/" target="_blank" rel="noopener noreferrer">
+            <LinkedIn className="mx-auto" />
           </a>
-          <a href="/">
-            <Mail className="lg:mx-10 mx-4" />
+          <a href="mailto:community@githubsrm.tech">
+            <Mail className="mx-auto" />
           </a>
         </div>
       </div>
