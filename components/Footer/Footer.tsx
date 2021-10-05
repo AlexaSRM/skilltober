597229c63de6aaa5750e2132b89265b461789e38
Vashist-Agarwalla <vashist1202@gmail.com>
2021-10-05 08:47:49 +0530

    💄 Responsive Footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -4,31 +4,31 @@ import Mail from '../../public/assests/Mail.svg'
 
 const Footer = () => {
     return(
-        <div className='flex text-TextMain text-center py-6'>
-            <div className='flex-1 mx-10'>
+        <div className='lg:flex text-TextMain text-center py-6'>
+            <div className='lg:flex-1 mx-10'>
                 <h1 className='text-2xl'>Alexa Dev SRM</h1>
                 <h2 className='text-md'>alexadevsrm.com</h2>
-                <div className='grid grid-cols-3 mt-4 px-10'>
-                    <Instagram className='mx-10'/>
-                    <LinkedIn className='mx-10'/>
-                    <Mail className='mx-10'/>
+                <div className='grid grid-cols-3 mt-4 px-14'>
+                    <Instagram className='lg:mx-10 mx-4'/>
+                    <LinkedIn className='lg:mx-10 mx-4'/>
+                    <Mail className='lg:mx-10 mx-4'/>
                 </div>
             </div>
-            <div className='flex-1 mx-10'>
+            <div className='lg:flex-1 lg:mt-0 mt-5 mx-10'>
                 <h1 className='text-2xl'>School of Computing</h1>
                 <h2 className='text-md'>SRM Institute of Science and Technology</h2>
-                <div className='grid grid-cols-2 mt-4 px-20'>
-                    <Instagram className='mx-10'/>
-                    <LinkedIn className='mx-10'/>
+                <div className='grid grid-cols-2 mt-4 px-24'>
+                    <Instagram className='lg:mx-10 mx-4'/>
+                    <LinkedIn className='lg:mx-10 mx-4'/>
                 </div>
             </div>
-            <div className='flex-1 mx-10'>
+            <div className='lg:flex-1 lg:mt-0 mt-5 mx-10'>
                 <h1 className='text-2xl'>Github Community SRM</h1>
                 <h2 className='text-md'>githubsrm.tech</h2>
-                <div className='grid grid-cols-3 mt-4 px-10'>
-                    <Instagram className='mx-10'/>
-                    <LinkedIn className='mx-10'/>
-                    <Mail className='mx-10'/>
+                <div className='grid grid-cols-3 mt-4 px-14'>
+                    <Instagram className='lg:mx-10 mx-4'/>
+                    <LinkedIn className='lg:mx-10 mx-4'/>
+                    <Mail className='lg:mx-10 mx-4'/>
                 </div>
             </div>
         </div>
