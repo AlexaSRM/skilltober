67e949c0bbb36712ebde7a0d1923fac6804517a1
Vashist-Agarwalla <vashist1202@gmail.com>
2021-10-05 08:12:25 +0530

    💄 Footer Structure

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,20 @@
+const Footer = () => {
+    return(
+        <div className='flex text-TextMain text-center py-6'>
+            <div className='flex-1 mx-10'>
+                <h1 className='text-2xl'>Alexa Dev SRM</h1>
+                <h2 className='text-md'>alexadevsrm.com</h2>
+            </div>
+            <div className='flex-1 mx-10'>
+                <h1 className='text-2xl'>School of Computing</h1>
+                <h2 className='text-md'>SRM Institute of Science and Technology</h2>
+            </div>
+            <div className='flex-1 mx-10'>
+                <h1 className='text-2xl'>Github Community SRM</h1>
+                <h2 className='text-md'>githubsrm.tech</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
