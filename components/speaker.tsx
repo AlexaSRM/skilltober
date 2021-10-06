01587fe469bfcd39bb2fa32be5d2f08d35bbd77b
Aniruddha Chatterjee <ruddha.mine@gmail.com>
2021-10-06 09:18:33 +0530

    :sparkles: Add responsive speaker component

---
+++ components/speaker.tsx
@@ -0,0 +1,38 @@
+import React, { FunctionComponent } from "react";
+
+type SpeakerPropsType = {
+  name: string;
+  designation: string;
+  description: string;
+  imagePath: string;
+};
+
+const Speaker: FunctionComponent<SpeakerPropsType> = ({
+  name,
+  designation,
+  description,
+  imagePath,
+}) => {
+  return (
+    <div className="flex flex-col   px-6 border-2 border-white rounded-2xl">
+      <div className="flex lg:flex-row flex-col ">
+        <div className="m-auto pt-8 lg:pt-0">
+          <img
+            src={imagePath}
+            className="rounded-full w-24 md:w-36 lg:w-44 xl:w-80"
+            alt={`${name} Image`}
+          />
+        </div>
+        <div className="flex flex-col lg:ml-10 pr-10 pl-6">
+          <h1 className="pt-6 pb-3 md:text-3xl text-xl">{name}</h1>
+          <h6 className="text-red-600 pb-5 md:text-base text-sm">
+            {designation}
+          </h6>
+          <p className="pb-6 md:text-lg text-base ">{description}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Speaker;
