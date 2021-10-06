f264f076f7a70d36ccaab300a84352b78ac4e86e
sbiswas2209 <sbiswas2209@outlook.com>
2021-10-06 21:36:30 +0530

    🐛 Add Log Level

--- services/shared/config.ts
+++ services/shared/config.ts
@@ -3,12 +3,14 @@ export interface Config {
   secretAccessKey: string;
   replyToAddress: string;
   domain: string;
+  debugLevel: boolean;
 }
 const config: Config = {
   accessKey: process.env.ACCESS_KEY_ID as string,
   secretAccessKey: process.env.ACCESS_KEY_SECRET as string,
   replyToAddress: process.env.REPLY_TO_ADDRESS as string,
   domain: process.env.DOMAIN as string,
+  debugLevel: process.env.LOG_LEVEL as string === 'prod' ? false : true
 };
 
 export default config;
